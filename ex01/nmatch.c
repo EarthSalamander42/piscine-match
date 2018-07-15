ec77c1ff625b0ce652b76bddd044005d3090b16f
Jason MAGUIRE <jmaguire@e3r13p5.42.fr>
2018-07-15 15:44:48 +0200

    Fixed ex01 had a main function.

--- ex01/nmatch.c
+++ ex01/nmatch.c
@@ -10,29 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 int		nmatch(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else if (*s1 == '\0' || *s2 == '\0')
+	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
-	else if (*s1 == *s2)
+	if (*s1 == *s2)
 		return (nmatch(s1 + 1, s2 + 1));
-	else if (*s2 == '*')
+	if (*s2 == '*')
 		return (nmatch(s1 + 1, s2) + nmatch(s1 + 1, s2 + 1));
 	else
 		return (0);
-}
-
-int		main(void)
-{
-	char *s1;
-	char *s2;
-
-	s1 = "bonjour";
-	s2 = "bo*jour";
-	printf("Result: %d", nmatch(s1, s2));
-	return (0);
 }
